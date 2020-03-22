5939ff2feb4df603276c2a5ecad042a66b351a77
Bart Sakowski <bart.sakowski@gmail.com>
2020-03-22 02:06:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -86,7 +86,7 @@ def reduce_to_total(source_array, starting_point=0)
 def reduce_to_all_true(source_array)
   i = 0
     while i < source_array.length do
-      return false if source[1] == false
+      return false if source_array[1] == false
       i += 1
     end
   new_value
